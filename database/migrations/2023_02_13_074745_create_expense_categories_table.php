c5ac4fefc78cd08cdf4e14063d61fa4cdf0ffd72
winwilust <winwilust@gmail.com>
2023-02-13 09:49:52 +0200

    add expensecategories table

---
+++ database/migrations/2023_02_13_074745_create_expense_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expense_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("type");
+            $table->string("applied_for");
+            $table->string("building_expense");
+            $table->string("non_building_expense");
+            $table->string("active");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expense_categories');
+    }
+};
